922f4a2803b58318d4b3ef12995c4efbf8bef8b0
Eyouale <teyouale@gmail.com>
2023-03-08 09:33:32 +0300

    Check-In Ui

--- app/Slack_APP/checkIn/shortcuts/__init__.py
+++ app/Slack_APP/checkIn/shortcuts/__init__.py
@@ -1,13 +1,15 @@
 import os
 import time
 from slack_sdk.errors import SlackApiError
-from app.Slack_APP.checkIn.views import get_checkIn_menu
+from app.Slack_APP.checkIn.views import get_checkIn_form
 
 
-def handle_checkIn_shortcut(ack, body, logger, client):
+def handle_checkIn_shortcut(ack, body, logger, client, context):
+    context['flask_app'].app_context().push()
     ack()
     logger.info(body)
-    client.views_open(trigger_id=body['trigger_id'], view=get_checkIn_menu())
+    client.views_open(
+        trigger_id=body['trigger_id'], view=get_checkIn_form(body))
 
 
 def register_shortcuts(app):
