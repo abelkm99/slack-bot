fab838a7058db6221f8e44d274b955e4c5c683a2
abelkm99 <abelkidanemariam99@gmail.com>
2023-03-13 20:00:30 +0300

    remove unused imports

--- app/Slack_APP/__init__.py
+++ app/Slack_APP/__init__.py
@@ -1,11 +1,7 @@
-import datetime
 from slack_bolt.adapter.flask import SlackRequestHandler
-from threading import local
 from flask import current_app
 from slack_bolt import App
 from app.Slack_APP.checkIn import register_checkIn_features
-from app.Slack_APP.dailyPlan.views.add_daily_plan import get_daily_plan_view
-from app.Slack_APP.dailyPlan.views.daily_plan_attachement import build_daily_plan_attachement
 from app.Slack_APP.profile import register_profile_features
 from app.Slack_APP.project import register_project_features
 from app.Slack_APP.dailyPlan import register_daily_plan_features
@@ -22,7 +18,6 @@ slack_app = App(
     signing_secret=CONFIG.SLACK_SIGNING_SECRET,
 )
 
-local_storage = local()
 
 
 @slack_app.middleware
@@ -46,12 +41,7 @@ register_daily_plan_features(app=slack_app)
 
 @slack_app.shortcut("check_modal")
 def handle_shortcuts(ack, shortcut, body, logger, client, context):
-    context['flask_app'].app_context().push()
     ack()
-    logger.debug(shortcut)
-    logger.debug(body)
-    client.views_open(
-        trigger_id=body['trigger_id'], view=get_daily_plan_view(body['user']['id']))
 
 
 handler = SlackRequestHandler(app=slack_app)
